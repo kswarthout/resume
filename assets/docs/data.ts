09390ddbde3be1b2a2b1bd223dc7448b898983c4
Kari Portfolio <kari.swarthout@gmail.com>
2020-02-07 19:38:28 +0800

    Auto-generated commit

---
+++ assets/docs/data.ts
@@ -0,0 +1,33 @@
+export const content = {
+    projects: [
+        {
+            title: 'Time & Expense Entry Web Suite | Olympic Systems',
+            description: `Rewrote a time and expense entry system converting it from a deprecated Silverlight-based implementation to the Angular framework. Along with another developer, I helped migrate existing data services to a C# Web API. I used the Angular framework to convert the frontend, and used NGXS to implement a Redux pattern for state management.`,
+            technologies: `C#, Typescript, HTML SCSS, SQL, Angular, Git / GitHub`,
+            repo: ''
+        },
+        {
+            title: 'Reporting Utility | Olympic Systems',
+            description: `Created a SQL reporting utility that integrates with Microsoft Dynamics GP and related business applications. The reports are created using DevExpress, a .NET-based component library, and are delivered using standard C# and WPF components. The app allows users to add, modify, and design reports, and store them centrally in GP’s company databases. Reports are available to users based on existing security privileges, and are viewable from web and desktop platforms.`,
+            technologies: `C#, .NET, SQL, Dexterity / SanScript, XAML, Visual Source Safe`,
+        },
+        {
+            title: `Cloud Practicum | UST Global's Employee On-Boarding API`,
+            description: `Worked with a team of 20 to develop a task-management web app for UST Global’s employee on-boarding process. As a member of the API team, I collaborated with frontend, backend, and devops teams to design a system that could be deployed to Azure using a CI/CD pipeline. Utilizing Swagger’s templating tools, I worked with the front-end team to design the API’s endpoints, and contributed to the development of both the API and authorization system. `,
+            technologies: `C#, bash, Azure, Git / GitHub`,
+            repo: `https://github.com/kswarthout/nscad-2018-cloud-practicum`
+        },
+        {
+            title: 'Android Practicum | Seattle DESC Dispatch System',
+            description: `Worked with a team of 5 to develop an Android app for Seattle’s DESC mobile crisis teams. The app was designed to dispatch team members and track their travel and response times for each incident. This data is captured in a Google Firebase realtime database, and could later be used for reporting purposes. We also used the database to track a user’s position and trigger cloud functions as necessary to share changes between users in real time.`,
+            technologies: `Java, XML, Git / GitHub, Google Firebase`,
+            repo: 'https://github.com/kswarthout/Dispatch'
+        },
+        {
+            title: 'Web Practicum | NSC Teacher Improvement Assessment',
+            description: `With a team of 15, designed and implemented a web application for North Seattle College teachers and administrators to capture responses to a teaching assessment. As the frontend team lead I managed the workloads for each week's sprint, and contributed to the UI development.`,
+            technologies: `PHP, SQL, HTML5 / CSS, Javascript Git / GitHub`,
+            repo: `https://github.com/kswarthout/NSC-AD410-TIP-GROUP2`
+        }
+    ]
+}
