feb1dee0730446f87c460ebafc7ae2458706d2df
kbullard <kari@projectcost.net>
2020-02-07 19:51:04 +0800

    removed firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyA47B0cQjcpzl_BEKOBSYwNmrc172HbzcA",
-    authDomain: "portfolio-site-bbea8.firebaseapp.com",
-    databaseURL: "https://portfolio-site-bbea8.firebaseio.com",
-    projectId: "portfolio-site-bbea8",
-    storageBucket: "portfolio-site-bbea8.appspot.com",
-    messagingSenderId: "425262560803",
-    appId: "1:425262560803:web:7d590bb8eff43d0e606396",
-    measurementId: "G-GY335Y8BNM"
-  }
+  production: false
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
